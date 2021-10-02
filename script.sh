c59f39ddd73f2a65cc71f8e30fb785eb6f9ae8bd
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2021-10-02 22:40:15 +1000

    Added tab indentation into messages

--- script.sh
+++ script.sh
@@ -18,20 +18,20 @@ NoColor='\033[0m'
 
 # Updating the system packages to make sure you have the correct versions of everything
 runSystemUpdates() {
-    clear && echo -e "\n${DARK_GRAY}Updating System Repositories...${NoColor}" && sleep 3
+    clear && echo -e "\n\t${DARK_GRAY}Updating System Repositories...${NoColor}" && sleep 3
     apt update
-    clear && echo -e "\n${DARK_GRAY}Updating System...${NoColor}" && sleep 3
+    clear && echo -e "\n\t${DARK_GRAY}Updating System...${NoColor}" && sleep 3
     apt upgrade -y
 }
 
 installDocker() {
-    clear && echo -e "\n${DARK_GRAY}Installing Docker Dependencies...${NoColor}" && sleep 3
+    clear && echo -e "\n\t${DARK_GRAY}Installing Docker Dependencies...${NoColor}" && sleep 3
     apt install apt-transport-https ca-certificates curl software-properties-common -y
-    clear && echo -e "\n${DARK_GRAY}Adding Docker Updated Repositories...${NoColor}" && sleep 3
+    clear && echo -e "\n\t${DARK_GRAY}Adding Docker Updated Repositories...${NoColor}" && sleep 3
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
     apt-cache policy docker-ce
-    clear && echo -e "\n${DARK_GRAY}Installing Docker and Docker-Compose...${NoColor}" && sleep 3
+    clear && echo -e "\n\t${DARK_GRAY}Installing Docker and Docker-Compose...${NoColor}" && sleep 3
     apt install docker-ce docker-compose
 }
 
